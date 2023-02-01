a741984d0e975ff83d18e983bbec3fb9fd3cea03
jvc223 <63932879+jvc223@users.noreply.github.com>
2023-01-31 16:25:07 -0800

    Add files via upload

---
+++ js/lebronscript.js
@@ -0,0 +1,14 @@
+/* script.js 
+   Author:
+   Date:
+*/
+
+
+
+const kareem = new CountUp('kareem', 0, 38387);
+
+kareem.start();
+
+const lebron = new CountUp('lebron', 0, 38271);
+
+lebron.start();
